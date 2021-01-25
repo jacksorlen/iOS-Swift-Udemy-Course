af801e4316214e4df7543e7ba03e69f8eec9c04e
Yevhenii Orlov <jacksorlen@icloud.com>
2021-01-25 22:58:43 +0200

    Add few projects made with Udemy Course by The App Brewery

---
+++ 2. I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Yevhenii Orlov on 14.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
